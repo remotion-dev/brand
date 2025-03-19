944f0c2ce6bd40d5fd126afc728d6ba6cb0809f2
Jonny Burger <jonathanburger11@gmail.com>
2025-03-19 12:18:10 +0100

    fixing the E

--- src/animated-logo/e.tsx
+++ src/animated-logo/e.tsx
@@ -10,7 +10,16 @@ const d = reverse.reverse(
 	'M874 421.5C874 456.696 845.668 485 811 485C776.332 485 748 456.696 748 421.5C748 386.304 776.332 358 811 358C845.668 358 874 386.304 874 421.5Z'
 );
 
-const d1 = reverse.reverse('M896 421.5 L 764 421.5');
+const middleLine = 421.5;
+const middleStrokeWidth = 32;
+
+const d1 = reverse.reverse(`M897 ${middleLine} L 764 ${middleLine}`);
+
+const horizontalMask = `M897 ${middleLine} L 860 ${
+	middleLine - middleStrokeWidth / 2
+} L 764 ${middleLine - middleStrokeWidth / 2} L 764 ${
+	middleLine + middleStrokeWidth / 2
+} L 897 ${middleLine + middleStrokeWidth / 2}`;
 
 export const E: React.FC<{
 	style?: React.CSSProperties;
@@ -58,13 +67,28 @@ export const E: React.FC<{
 				transform: `translateX(0) rotate(${rotate}deg)`,
 			}}
 		>
-			<path
-				d={d1}
-				stroke="currentcolor"
-				strokeWidth={32}
-				strokeDasharray={d1StrokeDashArray}
-				strokeDashoffset={d1StrokeDashoffset}
-			/>
+			<mask
+				id="horizontal-mask"
+				style={{
+					maskType: 'alpha',
+				}}
+				maskUnits="userSpaceOnUse"
+				x="700"
+				y="315"
+				width="209"
+				height="228"
+			>
+				<path d={horizontalMask} fill="currentcolor" />
+			</mask>
+			<g mask="url(#horizontal-mask)">
+				<path
+					d={d1}
+					stroke="currentcolor"
+					strokeWidth={32}
+					strokeDasharray={d1StrokeDashArray}
+					strokeDashoffset={d1StrokeDashoffset}
+				/>
+			</g>
 			<mask
 				id="mask1_108_2"
 				style={{
@@ -78,7 +102,7 @@ export const E: React.FC<{
 			>
 				<path
 					d="M835 422H900.5L909 320.5L876.5 315L709.5 320.5L700 543H851L903 452.5H835V422Z"
-					fill="black"
+					fill="currentcolor"
 				/>
 			</mask>
 			<g mask="url(#mask1_108_2)">
@@ -89,7 +113,7 @@ export const E: React.FC<{
 					stroke="currentcolor"
 					strokeWidth="46"
 				/>
-			</g>
+			</g>{' '}
 		</g>
 	);
 };
